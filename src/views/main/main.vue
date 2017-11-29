5b6c2a6986ede8e7cb22ed50cc95cabdb148d43d
谢凯峰 <xiekaifeng4042@tops001.com>
2017-11-29 18:04:16 +0800

    轮转图bug修改

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -389,8 +389,6 @@
     mounted() {
       new Swiper('.content-s2', {
         autoplay: 5000,
-        direction: 'horizontal',
-        loop: true,
         observer: true, //修改swiper自己或子元素时，自动初始化swiper
         observeParents: true, //修改swiper的父元素时，自动初始化swiper
         // 如果需要分页器
